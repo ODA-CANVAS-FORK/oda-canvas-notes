948976ef1d5d11c621aef2ef6a06ce8fc0078e92
Ferenc Hechler <ferenc.hechler@telekom.de>
2025-03-11 13:27:45 +0100

    add curl bash and jq to echoservice docker image

--- apps/echoservice/build.sh
+++ apps/echoservice/build.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-docker build --build-arg=http_proxy=$http_proxy --build-arg=https_proxy=$https_proxy -t ocfork/echoservice:v0.1.1-rc .
-docker tag ocfork/echoservice:v0.1.1-rc ocfork/echoservice:latest
-docker push ocfork/echoservice:v0.1.1-rc
+docker build --build-arg=http_proxy=$http_proxy --build-arg=https_proxy=$https_proxy -t ocfork/echoservice:v0.1.2-rc .
+docker tag ocfork/echoservice:v0.1.2-rc ocfork/echoservice:latest
+docker push ocfork/echoservice:v0.1.2-rc
 docker push ocfork/echoservice:latest
